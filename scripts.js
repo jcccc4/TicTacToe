689f712fe5226ab61bf98be9ca7ef2b867c75172
jcccc4 <jcsacra4@gmail.com>
2022-04-16 22:51:40 +0800

    initial procedure is done

--- scripts.js
+++ scripts.js
@@ -6,10 +6,11 @@
 let playerTurn = true;
 let turn = 0;
 const arr = [];
+const display = document.getElementById('myModal');
 
 function Board() {
     const board = document.getElementById('ticBoard');
-
+    const span = document.getElementsByClassName("close")[0];
 
     for (let row = 0; row < 3; row++) {
         const box = [];
@@ -43,36 +44,60 @@ function Board() {
             });
         }
     }
+    span.onclick = function () {
+        display.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+        console.log(event.target);
+        if (event.target == display) {
+            display.style.display = "none";
+        }
+    };
 }
-function boardChecker(sign) {
+function boardChecker(sign, name) {
     let checker = document.querySelectorAll('.tile');
+    let winner = document.getElementsByClassName('modal-content');
+
+    const winText = document.createElement('div');
+
     checker.forEach(element => {
         element.addEventListener('click', () => {
             for (let i = 0; i < 3; i++) {
                 if (arr[i].every(x => x[1] === sign)) {
-                    console.log(`${sign} is the winner row = ${i}`);
+                    console.log(`${name} is the winner row = ${i}`);
+
+
+                    display.display = 'flex';
+                    winner[0].appendChild(winText);
                 }
             }
             for (let i = 0; i < 3; i++) {
                 if (arr.every(x => x[i][1] === sign)) {
-                    console.log(`${sign} is the winner column = ${i}`);
+                    console.log(`${name} is the winner column = ${i}`);
+                    winText.innerHTML = "YOU WIN asdf";
+                    display.style.display = 'flex';
+                    winner[0].appendChild(winText);
                 }
             }
             if (arr.every((x, y) => x[y][1] === sign)) {
                 console.log(`${sign} is the winner diag`);
+                winText.innerHTML = "YOU WIN asdf";
+                display.style.display = 'flex';
+                winner[0].appendChild(winText);
             }
             else if (arr[0][2][1] === sign && arr[1][1][1] === sign && arr[2][0][1] === sign) {
-                console.log(`${sign} is the winner diag`);
+                console.log(`${name} is the winner diag`);
+                winText.innerHTML = "YOU WIN asdf";
+                winner[0].app.styleendChild(winText);
             }
             else if (turn === 9) {
                 console.log(`it is a tie`);
             }
-            // arr[i].every(x => x[0] === 'X');
-
         });
     });
 }
 
 Board();
-boardChecker('X');
-boardChecker('O');
+boardChecker('X', 'Player1');
+boardChecker('O', 'Player2');
